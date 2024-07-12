025c2851db82a8fdb0a4a403197d5df89eaf3581
조준호 <81681883+khanturtle@users.noreply.github.com>
2024-07-12 18:08:32 +0900

    Title: 데이터 교환, Time: 81ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240712/데이터 교환/exchange-data.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a=5,b=6,c=7;
+        b=a;
+        c=b;
+        a=c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.print(c);
+    }
+}
