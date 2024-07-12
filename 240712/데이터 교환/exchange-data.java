32c0a7495d8bb0519cfe0af843961a47f1e02453
조준호 <81681883+khanturtle@users.noreply.github.com>
2024-07-12 18:09:29 +0900

    Title: 데이터 교환, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

--- 240712/데이터 교환/exchange-data.java
+++ 240712/데이터 교환/exchange-data.java
@@ -2,11 +2,12 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         int a=5,b=6,c=7;
-        b=a;
-        c=b;
-        a=c;
-        System.out.println(a);
-        System.out.println(b);
-        System.out.print(c);
+        int ta,tb,tc;
+        tb=a;
+        tc=b;
+        ta=c;
+        System.out.println(ta);
+        System.out.println(tb);
+        System.out.print(tc);
     }
 }
