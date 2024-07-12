10ecfe3ee095b91da11915e110fd66a062296090
조준호 <81681883+khanturtle@users.noreply.github.com>
2024-07-12 18:10:55 +0900

    Title: 합을 복사하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240712/합을 복사하기/copy-the-sum.java
+++ 240712/합을 복사하기/copy-the-sum.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         int a=1,b=2,c=3;
-        int sum = a+b+c;
+        int sum = 0;
+        sum=a+b+c;
         a=sum;
         b=sum;
         c=sum;
