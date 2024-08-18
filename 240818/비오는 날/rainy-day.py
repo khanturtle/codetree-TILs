3c1f2ec1afb0e014132770fb3a0e8d46fdb152f1
조준호 <81681883+khanturtle@users.noreply.github.com>
2024-08-18 15:43:47 +0900

    Title: 비오는 날, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240818/비오는 날/rainy-day.py
@@ -0,0 +1,22 @@
+class day_info:
+    def __init__(self,day,yoil,weather):
+        self.day=day
+        self.yoil=yoil
+        self.weather=weather
+
+n= int(input())
+days=[
+    tuple(input().split())
+    for _ in range(n)
+]
+
+rainy_days=[]
+for d in days:
+    if(d[2]=='Rain'):
+        rainy_days.append(d)
+
+rainy_days = sorted(rainy_days, key=lambda rainy_day: rainy_day[0])
+
+print(rainy_days[0][0],end=' ')
+print(rainy_days[0][1],end=' ')
+print(rainy_days[0][2],end=' ')
